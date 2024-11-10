190ad6a7579a94b6795fc6e6a805817ed199306c
Adrien <adrien.jacquin@eleves.ec-nantes.fr>
2024-11-10 16:07:46 -0500

    Cycle manager

--- Assets/Scripts/GameEnd.cs
+++ Assets/Scripts/GameEnd.cs
@@ -1,13 +1,29 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameEnd : MonoBehaviour
 {
+    public UnityEvent OnTriggerWitchLaugh, OnQuitPanel;
+
+    void Start()
+    {
+        StartCoroutine(TriggerWitchLaughCoroutine());
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Destroy(gameObject);
+            OnQuitPanel?.Invoke();
+            gameObject.SetActive(false);
         }
+    }
+
+    IEnumerator TriggerWitchLaughCoroutine()
+    {
+        yield return new WaitForSeconds(2f);
+        OnTriggerWitchLaugh?.Invoke();
     }
 }
